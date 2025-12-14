327c90209c54a2df40e7d96f3c756286f3afcd23
Taif AlQarni <132124981+taifaau@users.noreply.github.com>
2025-12-14 10:48:26 +0300

    Update FileHandling01.py

--- FileHandling01.py
+++ FileHandling01.py
@@ -1,11 +1,11 @@
 '''
+Task 01
 Read the file named server_log.txt.
 Find every line that contains the word "ERROR".
 Write only those lines into a new file called errors_only.txt.
 Bonus: At the end, print how many errors were found
 '''
 #Taif AlQarni
-#import os
 
 #have two file read from server_log and create/write to errors.txt file
 with open('server_log.txt', 'r') as s, open('errors.txt', 'w') as e:
@@ -14,10 +14,11 @@ with open('server_log.txt', 'r') as s, open('errors.txt', 'w') as e:
         #for-loop here to read every line in the file and search for line start with word'ERROR'
         if line.startswith('ERROR'):
             e.write(line)
-            Errorlines += 1 #to count all lines read from the loop
+            Errorlines += 1 #to count all lines read using the loop
 
 
 print('Errors Found: {}'.format(Errorlines))
+
 
 
 
